548397cf49b1774db4a8f99fba57622bfb7a00d9
Robert Frentz <frentzrobert@yahoo.com>
2021-02-20 14:16:00 +0200

    Created scene with small physics demo

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    public float speed;
+    private Rigidbody playerRB;
+    private Vector3 movement;
+    void Start()
+    {
+        playerRB = player.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement = new Vector3(horizontal, 0f, vertical).normalized;
+        Debug.Log(movement);
+        playerRB.MovePosition(player.transform.position + (movement * speed * Time.deltaTime));
+    }
+}
