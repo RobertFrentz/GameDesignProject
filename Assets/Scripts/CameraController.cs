0bd1f75f887044101a22a8435d4e7e55843d8037
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-03-07 17:58:52 +0200

    mde camera for ball and car

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject car;
+    public GameObject cameraRig;
+    public GameObject ballRig;
+    public GameObject target;
+    public GameObject ball;
+
+    // Update is called once per frame
+    void BallCam()
+    {
+        cam.transform.position = ballRig.transform.position;
+        cam.transform.LookAt(ball.transform.position);
+
+    }
+    void CarCam()
+    {
+        cam.transform.position = cameraRig.transform.position;
+        cam.transform.LookAt(car.transform.position);
+    }
+    void Update()
+    {
+        //CarCam();
+        BallCam();
+    }
+}
