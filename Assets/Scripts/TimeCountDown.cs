a6f1192b0777ca6cea3b01e679b610cf157e4e5b
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-03-22 18:39:54 +0200

    countdown

---
+++ Assets/Scripts/TimeCountDown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountDown : MonoBehaviour
+{
+    public GameObject textDisplay;
+    public int secondLeft = 3;
+    public bool takingAway = false;
+
+    public void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "" + secondLeft;
+    }
+    public void Update()
+    {
+        if(takingAway == false && secondLeft > 0)
+        {
+            
+            StartCoroutine(TimerTake());
+        }
+        else if(takingAway == false && secondLeft < 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+
+    public IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondLeft -= 1;
+        if (secondLeft == 0)
+        {
+            textDisplay.GetComponent<Text>().text = "" ;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "" + secondLeft;
+        }
+        
+        takingAway = false;
+    }
+}
