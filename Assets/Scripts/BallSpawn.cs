6011a48237204eb0a51ad2db437396787eecc3bd
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-03-22 21:22:57 +0200

    score logic implemented

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject ballSpawn;
     public GameObject ball;
-    public ScoreChanger scoreChanger;
+    public Text redScore;
+    public Text blueScore;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,7 +18,17 @@ public class BallSpawn : MonoBehaviour
             ball.transform.position = ballSpawn.transform.position;
             ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
             ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            scoreChanger.ScoreGoal();
+            
+            if(name == "GoalCollider1")
+            {
+                int currentRedScoreValue = int.Parse(redScore.text);
+                redScore.text = (currentRedScoreValue + 1).ToString();
+            }
+            else if (name == "GoalCollider2")
+            {
+                int currentBlueScoreValue = int.Parse(blueScore.text);
+                blueScore.text = (currentBlueScoreValue + 1).ToString();
+            }
         }
     }
 }
