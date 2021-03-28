38d6716054d57f334e0e899a26e3a1d0a397d6ac
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-03-28 17:55:32 +0300

    added buttons for play, exit and nickname field

---
+++ Assets/Scripts/NicknameInputField.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NicknameInputField : MonoBehaviour
+{
+
+    InputField inputField;
+    Graphic placeholder;
+    Text inputText;
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+        placeholder = inputField.placeholder;
+        inputText = GameObject.FindObjectsOfType<Text>()[0];
+    }
+    void Update() {
+
+        
+        
+    }
+    void OnGUI()
+    { 
+        if (Event.current.isKey && Event.current.type == EventType.KeyDown)
+        {
+            ((Text)placeholder).text = "";
+            if (Event.current.keyCode.ToString() != "None")
+            {
+                inputText.text += Event.current.keyCode.ToString();
+                Debug.Log(Event.current.keyCode);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayUIButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayUIButton : MonoBehaviour
+{
+    Button buttonToHide;
+    InputField nickNameField;
+    void Start()
+    {
+        buttonToHide = GetComponent<Button>();
+        nickNameField = GameObject.FindObjectsOfType<InputField>()[0];
+        Button[] all_Objs = GameObject.FindObjectsOfType<Button>();
+        
+        nickNameField.gameObject.SetActive(false);
+        buttonToHide.onClick.AddListener(() => ButtonClicked(all_Objs));
+    }
+
+
+    void ButtonClicked(Button[]  all_objects)
+    {
+        foreach(Button b in all_objects)
+        {
+            if(b.name == "PlayButton" || b.name == "ExitButton")
+            {
+                b.gameObject.SetActive(false);
+            }
+           
+        }
+        nickNameField.gameObject.SetActive(true);
+    }
+}
