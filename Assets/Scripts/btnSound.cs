af5f00da66b741280ccf146bbed42ce79ee826c7
Tugui Madalina <tuguimadalinaa@gmail.com>
2021-06-02 17:30:27 +0300

    added volume slider + button sound

---
+++ Assets/Scripts/btnSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnSound : MonoBehaviour
+{
+    	public AudioSource hoverS;
+    	public AudioSource clickS;
+    	public void HoverSound() {
+		hoverS.Play();
+	}
+    	public void ClickSound(){
+		clickS.Play();
+    	}
+
+}
